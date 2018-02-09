b7e453b6911380a21d39f71334ac7f57eba0407b
Ari <alk511@ad.nyu.edu>
2018-02-09 16:04:28 -0500

    2/9/18

--- js/script.js
+++ js/script.js
@@ -1,11 +1,4 @@
 var map = L.map('my-map').setView([40.713435,-73.971291], 12);
-  // L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}.{ext}', {
-  // 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-  // 	subdomains: 'abcd',
-  // 	minZoom: 0,
-  // 	maxZoom: 20,
-  // 	ext: 'png'
-  // }).addTo(map);
   L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_nolabels/{z}/{x}/{y}.png', {
   	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
   	subdomains: 'abcd',
@@ -24,3 +17,4 @@ carData.forEach(function(carObject) {
   L.circleMarker(latlng, options).addTo(map)
       // .bindPopup(pizzaObject.name + ' likes to eat at ' +  pizzaObject.pizzaShop);
 });
+  //dkfjalskdfj
